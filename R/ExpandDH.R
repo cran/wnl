ddbcb8df0910fd11dbc0d39aee4f4dfe254813c7
Kyun-Seop Bae <k@acr.kr>
2025-03-23 03:50:02 +0000

    version 0.8.3

--- R/ExpandDH.R
+++ R/ExpandDH.R
@@ -11,20 +11,21 @@ ExpandDH = function(DH, Fo = 1)
   BlankRow[1, ] = rep(0, ncol(DH))
   RateDat = DH[DH$RATE > 0, ]
   nRate = nrow(RateDat)
-  for (i in 1:nRate) {
-    cRATE = RateDat[i, "RATE"]
-    cCMT = RateDat[i, "CMT"]
-    cTIME = RateDat[i, "TIME"]
-    cDUR = RateDat[i, "AMT"] / RateDat[i, "RATE"]
-    cEND = cTIME + cDUR
-    if (!(cEND %in% DH[, "TIME"])) {
-
-      BlankRow[1, "TIME"] = cEND
-      DH = rbind(DH, BlankRow)
-      DH = DH[order(DH$TIME), ]
+  if (nRate > 0) {
+    for (i in 1:nRate) {
+      cRATE = RateDat[i, "RATE"]
+      cCMT = RateDat[i, "CMT"]
+      cTIME = RateDat[i, "TIME"]
+      cDUR = RateDat[i, "AMT"] / RateDat[i, "RATE"]
+      cEND = cTIME + cDUR
+      if (!(cEND %in% DH[, "TIME"])) {
+        BlankRow[1, "TIME"] = cEND
+        DH = rbind(DH, BlankRow)
+        DH = DH[order(DH$TIME), ]
+      }
+      DH[DH$TIME >= cTIME & DH$TIME < (cTIME + cDUR), "RATE2"] = DH[DH$TIME >= cTIME & DH$TIME < (cTIME + cDUR), "RATE2"] + cRATE
+      DH[DH$TIME >= cTIME & DH$TIME < (cTIME + cDUR), "CMT"] = cCMT
     }
-    DH[DH$TIME >= cTIME & DH$TIME < (cTIME + cDUR), "RATE2"] = DH[DH$TIME >= cTIME & DH$TIME < (cTIME + cDUR), "RATE2"] + cRATE
-    DH[DH$TIME >= cTIME & DH$TIME < (cTIME + cDUR), "CMT"] = cCMT
   }
   return(DH)
 }
